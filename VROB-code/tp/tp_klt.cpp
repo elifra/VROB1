1b0e67bce24931301bfb84cbf5d9cab9442c9ee7
mael le vot <18007735@e206m06.istic.univ-rennes1.fr>
2021-11-16 17:38:52 +0100

    voisinnage

--- VROB-code/tp/tp_klt.cpp
+++ VROB-code/tp/tp_klt.cpp
@@ -33,11 +33,35 @@
 
 using namespace std;
 
+double sommeVoisinnage(vpImage<double> I, int i, int j, int tailleF) {
+    assert(tailleF > 0 && tailleF%2 != 0);
+    double val = 0;
+    for(int k = i - (int)(tailleF/2); k <= i + (int)(tailleF/2); k++) {
+        for(int l = j - (int)(tailleF/2); l <= j + (int)(tailleF/2); l++) {
+            if(k >=0 && k < I.getRows() && l >=0 && l < I.getCols()){
+                val += I[k][l];
+            }
+        }
+    }
+    return val;
+}
+
 vpImage<double> harris_detector(vpImage<unsigned char> I) {
     vpImage<double> IgradX;
     vpImageFilter::getGradX(I, IgradX);
     vpImage<double> IgradY;
     vpImageFilter::getGradY(I, IgradY);
+
+    vpImage<double> IgradXcarre = vpImage<double>(I.getRows(), I.getCols());
+    vpImage<double> IgradYcarre = vpImage<double>(I.getRows(), I.getCols());
+    vpImage<double> IgradXIgradY = vpImage<double>(I.getRows(), I.getCols());
+    for(int i = 0; i < I.getRows(); i++) {
+        for(int j = 0; j < I.getCols(); j++) {
+            IgradXcarre[i][j] = IgradX[i][j]*IgradX[i][j];
+            IgradYcarre[i][j] = IgradY[i][j]*IgradY[i][j];
+            IgradXIgradY[i][j] = IgradX[i][j]*IgradY[i][j];
+        }
+    }
 
     vpImage<double> R = vpImage<double>(I.getRows(), I.getCols());
     double k = 0.04;
@@ -73,7 +97,7 @@ int main(int argc, char **argv)
     vpImageIo::read(I1,"images/image.0001.pgm"); 
 
     vpImage<double> Iharris = harris_detector(I1);
-    afficheHarris(Iharris, 1000.0);
+    afficheHarris(Iharris, 100.0);
 }
 
 
